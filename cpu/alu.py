f214c9111446416be49cc573cfc64244b8f5f7bf
Tommi Kiiski <kiiski.tommi@hotmail.com>
2023-09-24 18:13:56 +0300

    Create cu and alu

---
+++ cpu/alu.py
@@ -0,0 +1,16 @@
+class Alu:
+    def __init__(self) -> None:
+        self.op1 = "0"
+        self.op2 = "0"
+        self.operand = ""
+
+    def count(self, operators):
+        pass
+
+    def add(self, operators):
+        pass
+
+    def subtract(self, operators):
+        pass
+
+
